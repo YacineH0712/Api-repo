c8725dd596d0c371bc6d35ff38d20178ca487cf3
YacineH0712 <104352145+YacineH0712@users.noreply.github.com>
2023-02-08 14:32:24 +0100

    Integration of the ORM

--- api.py
+++ api.py
@@ -1,76 +1,60 @@
 from flask import Flask, request
-import sqlite3
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///bank.db'
+db = SQLAlchemy(app)
 
-def get_db_connection():
-    return sqlite3.connect("bank.db")
+class Account(db.Model):
+    name = db.Column(db.String, primary_key=True)
+    amount = db.Column(db.Integer)
 
 @app.route('/api/v1/create', methods=["POST"])
 def create():
-    conn = get_db_connection()
-    c = conn.cursor()
     name = request.json['name']
     amount = request.json['amount']
-    c.execute("INSERT INTO accounts (name, amount) VALUES (?,?)", (name, amount))
-    conn.commit()
-    conn.close()
+    account = Account(name=name, amount=amount)
+    db.session.add(account)
+    db.session.commit()
     return f"Account created for {name} with initial amount of {amount}"
 
 @app.route('/api/v1/amount', methods=["GET"])
 def amount():
-    conn = get_db_connection()
-    c = conn.cursor()
     name = request.args.get('name')
-    c.execute("SELECT amount FROM accounts WHERE name=?", (name,))
-    result = c.fetchone()
-    conn.close()
-    if result:
-        return str(result[0])
+    account = Account.query.get(name)
+    if account:
+        return str(account.amount)
     else:
         return "No account found with the name " + name
 
 @app.route("/api/v1/deposit", methods=["POST"])
 def deposit():
-    conn = get_db_connection()
-    c = conn.cursor()
     name = request.json['name']
     amount = request.json['amount']
-    c.execute("UPDATE accounts SET amount = amount + ? WHERE name = ?", (amount, name))
-    conn.commit()
-    conn.close()
-    return f"Deposited {amount} to the account of {name}"
+    account = Account.query.get(name)
+    if account:
+        account.amount += amount
+        db.session.commit()
+        return f"Deposited {amount} to the account of {name}"
+    else:
+        return "No account found with the name " + name
 
 @app.route("/api/v1/withdraw", methods=["POST"])
 def withdraw():
-    conn = get_db_connection()
-    c = conn.cursor()
     name = request.json['name']
     amount = request.json['amount']
-    c.execute("SELECT amount FROM accounts WHERE name = ?", (name,))
-    result = c.fetchone()
-    if result:
-        current_amount = result[0]
-        if amount > current_amount:
+    account = Account.query.get(name)
+    if account:
+        if amount > account.amount:
             return f"Insufficient amount in the account of {name}"
         else:
-            c.execute("UPDATE accounts SET amount = amount - ? WHERE name = ?", (amount, name))
-            conn.commit()
-            conn.close()
+            account.amount -= amount
+            db.session.commit()
             return f"Withdrew {amount} from the account of {name}"
     else:
-        conn.close()
         return "No account found with the name " + name
 
 if __name__ == "__main__":
-    conn = get_db_connection()
-    c = conn.cursor()
-    c.execute("""
-        CREATE TABLE IF NOT EXISTS accounts (
-            name TEXT PRIMARY KEY,
-            amount INTEGER 
-        )
-    """)
-    conn.commit()
-    conn.close()
-    app.run(debug=True)
+    with app.app_context():
+        db.create_all()
+        app.run(debug=True)
